68c7c7d3a96ae7c363e6f0ab4a41010c08d0793b
MateusSilvaFreitas <mateus.freitas7589@gmail.com>
2019-06-12 21:52:35 -0300

    Create redirecionamento.js

---
+++ resources/js/redirecionamento.js
@@ -0,0 +1,38 @@
+function redirecionaGraficoConsumo(){
+    window.location.href = "./graficoConsumo.html";
+}
+function redirecionaDespezas(){
+    window.location.href = "./cadastroDespeza.html";
+}
+
+function redirecionaCadastroProdutos(){
+    window.location.href = "./cadastroProdutos.html";
+}
+
+function redirecionaDespezasAnimal(){
+    window.location.href = "./cadastroDespeza.html";
+}
+function redirecionaGraficoEstoque(){
+    window.location.href = "./graficoEstoque.html";
+}
+function redirecionaCadastroAnimal(){
+    window.location.href = "./cadastroBoi.html";
+}
+function redirecionaGraficoDadosAnimal(){
+    window.location.href = "./graficoDadosAnimal.html";
+}
+function redirecionaAbatedouro(){
+    window.location.href = "./abatedouro.html";
+}
+function redirecionaAtividadesRealizadaAnimal(){
+    window.location.href = "./atividadesRealizadaAnimal.html";
+}
+function redirecionaGraficoLucros(){
+    window.location.href = "./graficoLucros.html";
+}
+function redirecionaTimeLine(){
+    window.location.href = "./timeLine.html";
+}
+function redirecionaInicio(){
+    window.location.href = "./index.html";
+}
