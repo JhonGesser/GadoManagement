16e05b1f65e9b93abf7666828e90be9af5dde320
MateusSilvaFreitas <mateus.freitas7589@gmail.com>
2019-06-25 19:03:49 -0300

    Update redirecionamento.js

--- resources/js/redirecionamento.js
+++ resources/js/redirecionamento.js
@@ -13,22 +13,22 @@ function redirecionaDespezasAnimal(){
     window.location.href = "./cadastroDespeza.html";
 }
 function redirecionaGraficoEstoque(){
-    window.location.href = "./graficoEstoque.html";
+    window.location.href = "./graficoConsumo.html";
 }
 function redirecionaCadastroAnimal(){
     window.location.href = "./cadastroBoi.html";
 }
 function redirecionaGraficoDadosAnimal(){
-    window.location.href = "./graficoDadosAnimal.html";
+    window.location.href = "./graficoAnimalFazenda.html";
 }
 function redirecionaAbatedouro(){
     window.location.href = "./abatedouro.html";
 }
 function redirecionaAtividadesRealizadaAnimal(){
-    window.location.href = "./atividadesRealizadaAnimal.html";
+    window.location.href = "./cadastroAtividadesRealizadas.html";
 }
 function redirecionaGraficoLucros(){
-    window.location.href = "./graficoLucros.html";
+    window.location.href = "./graficoGanhos.html";
 }
 function redirecionaTimeLine(){
     window.location.href = "./timeLine.html";
